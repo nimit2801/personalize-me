e5ddf75a3802edc12e5beede000e3345524d37aa
nimit2801 <mnimitsavant@outlook.com>
2021-07-20 00:21:33 +0530

    Server.js file complete ✅

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ const app = express();
 // Body Parser
 app.use(express.json());
 
+// importing middlewares
+// import { auth } from './middleware/auth.js';
+// app.use(auth);
 const PORT = process.env.PORT || 7000;
 
 // Route Files
 import tasks from './routes/task.js';
 import client from './routes/client.js';
 import taskClass from './routes/taskClass.js';
+import test from './routes/test.js';
 app.use('/api/v1/tasks', tasks);
 app.use('/api/v1/clients', client);
 app.use('/api/v1/taskclass', taskClass);
-
+app.use('/api/v1/test', test);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
