8b8137f4706ec7a8064a8d2915380c3ab59fdd82
nimit2801 <mnimitsavant@outlook.com>
2021-07-27 03:25:27 +0530

    deleted test route ❎

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const PORT = process.env.PORT || 7000;
 import tasks from './routes/task.js';
 import client from './routes/client.js';
 import taskClass from './routes/taskClass.js';
-import test from './routes/test.js';
+// import test from './routes/test.js';
 app.use('/api/v1/tasks', tasks);
 app.use('/api/v1/clients', client);
 app.use('/api/v1/taskclass', taskClass);
-app.use('/api/v1/test', test);
+// app.use('/api/v1/test', test);
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
