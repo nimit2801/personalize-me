637a73ea4c69c5052d57e32f967886b2e7e694fd
nimit2801 <mnimitsavant@outlook.com>
2021-07-31 07:07:35 +0530

    Connection Complete ✅

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config({path: "./database/config.env"});
-mongoose.connect(process.env.MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
-const connect = mongoose.connection;
-connect.on('error', console.error.bind(console, 'connect error:'));
-connect.once('open', function() {
-    console.log('We are connected to the database!');
-});
 
-export default connect;
+export const conn = async () => {
+    try{
+        await mongoose.connect(process.env.MONGOURI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        }),
+        console.log("Database connected!");
+    }
+    catch(e){
+        console.log(e);
+    }
+}
