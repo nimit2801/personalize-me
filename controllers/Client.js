075935e4b2307ef77e569a8153a9677437b8bd80
nimit2801 <mnimitsavant@outlook.com>
2021-08-04 03:02:25 +0530

    Add bcypt for password encryption #16 Client ✨

--- controllers/Client.js
+++ controllers/Client.js
@@ -1,14 +1,28 @@
 import {Client} from '../model/model.js';
-
+import bcrypt from 'bcrypt';
 // @desc create new client
 // @route POST /client
 // @access private
 
+function generateRandomString() {
+	let randomString = '';
+	const randomNumber = Math.floor(Math.random() * 10);
+
+	for (let i = 0; i < 20 + randomNumber; i++) {
+		randomString += String.fromCharCode(33 + Math.floor(Math.random() * 94));
+	}
+
+	return randomString;
+}
 export const createNewClient = async (req, res, next) => {
-    let random = '123asd123';
+    let random = await generateRandomString();
+    const password = await req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    let hash = await bcrypt.hash(password, salt);
+    console.log(hash);
     const newClient = await Client.create({
         userName: req.body.userName,
-        password: req.body.password,
+        password: hash,
         fullName: req.body.fullName,
         token: random,
     });
@@ -19,9 +33,28 @@ export const createNewClient = async (req, res, next) => {
 }
 
 export const getClient = async (req, res, next) => {
-    const client = await Client.findOne({token: req.headers.token});
-    if(!client) {
-        return res.status('404').json({success: false, message:'The client was not found!'});
+    let token = await req.headers.token;
+    if(token) {
+        const client = await Client.findOne({token: req.headers.token});
+        if(!client) {
+            return res.status('404').json({success: false, message:'The client was not found!'});
+        }
+        return res.status('200').json({message:'The client was found!', data: client});
     }
-    return res.status('200').json({message:'The client was found!', data: client});
+    else if(!token){
+        console.log('login');
+        const { userName, password } = await req.body;
+        const client = await Client.findOne({userName});
+        if(!client) {
+            console.log('The Client was not found please check your credentials and try again!');
+            return res.status('401').json({success: false, data: 'The Client was not found please check your credentials and try again!'})
+        }
+        else if(client){
+            const validPassword = await bcrypt.compare(password, client.password)
+            if(validPassword == false) {
+                return res.status('401').json({success: false, data: 'Incorrenct credentials please try again!'})
+            }
+        }
+        return res.status('200').json({success: true, data: client})
+    }
 }
