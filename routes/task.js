acc02bb4343783fb4af7ae6397594fe4defaef8f
nimit2801 <mnimitsavant@outlook.com>
2021-08-04 06:08:17 +0530

    Making the routes more secure #14

--- routes/task.js
+++ routes/task.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import { getAllTasksOfTaskClass, getAllTasksOfUser, createTask, getSingleTask, deleteSingleTask} from '../controllers/task.js';
 const router = express.Router();
-
-router.route('/:userName').get(getAllTasksOfUser);
-router.route(':userName/:taskClass').get(getAllTasksOfTaskClass);
-router.route('/').post(createTask)
-router.route('/userName/:id').get(getSingleTask).delete(deleteSingleTask)
+router.route('/:userName').get(getAllTasksOfUser).post(createTask);
+router.route('/:userName/:taskClass').get(getAllTasksOfTaskClass);
+router.route('/:userName/:taskClass').get(getSingleTask).delete(deleteSingleTask)
 
 export default router;
