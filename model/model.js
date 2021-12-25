9332355a6d4ef50511282a7ba64b82fd1ad65918
nimit2801 <mnimitsavant@outlook.com>
2021-12-26 02:44:59 +0530

    #27 Solved ✅

--- model/model.js
+++ model/model.js
@@ -3,30 +3,35 @@ const { Schema } = mongoose;
 
 const client = new Schema({
   userName: {type: String, required: true, unique: true},
+  providerId: {type: String, required: false, unique: true},
   fullName: { type: String, required: true },
   created: { type: Date, default: new Date},
-  password: { type: String, required: true},
-  token: {type: String, required: true, unique: true},
+  password: { type: String},
+  taskClasses: [taskClass],
+  // token: {type: String, required: true, unique: true},
 })
 
 const taskClass = new Schema({
-  token: {type: String, required: true},
-  userName: {type: String, ref: 'client'},
+  // token: {type: String, required: true},
+  // userName: {type: String, ref: 'client'},
   taskClass: {type: String, required: true},
+  tasks: [task],
   description: String,
   date: { type: Date, default: Date.now, required: true },
 });
 
 const task = new Schema({
-  userName: {type: String, ref: 'client'},
+  // userName: {type: String, ref: 'client'},
+  // taskClass: {type: String, ref: 'taskClass', required: true},
   taskName:  {type: String, required: true},
-  taskClass: {type: String, ref: 'taskClass', required: true},
   description: String,
+  isCompeted: {type: Boolean, default: false, required: true },
   date: { type: Date, default: Date.now, required: true },
   priority: { type: String, default: 'low', required: true },
   dueDate: { type: Date, default: Date.now },
 });
 
+
 export const Client = mongoose.model('Client', client);
-export const TaskClass = mongoose.model('TaskClass', taskClass);
-export const Task = mongoose.model('Task', task);
+// export const TaskClass = mongoose.model('TaskClass', taskClass);
+// export const Task = mongoose.model('Task', task);
